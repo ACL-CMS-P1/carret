f616290cb0dc22c968d7715ca1587e627f188ba5
christylaguardia <christinelaguardia@gmail.com>
2017-08-09 16:34:38 -0700

    added admin user patch route and tests

--- tests/e2e/admin.test.js
+++ tests/e2e/admin.test.js
@@ -24,33 +24,34 @@ describe.only('admin only options', () => {
         }
     ];
 
+    let admin = {
+        name: 'admin0',
+        email: 'zero@admin.com',
+        password: 'password0',
+        role: 'admin'
+    };
+
     function signup(user) {
         return request.post('/auth/signup')
             .send(user)
             .then(res => res.body);
     }
 
-    // function signin(user) {
-    //     return request.post('/auth/signin')
-    //         .send(user)
-    //         .then(res => res.body);
-    // }
+    function signin(user) {
+        return request.post('/auth/signin')
+            .send(user)
+            .then(res => res.body);
+    }
 
     before(db.drop);
-
+    before(() => signup(admin));
     before(() => Promise.all(users.map(signup)));
 
-    it('/users', () => {
-        let admin = {
-            name: 'admin0',
-            email: 'zero@admin.com',
-            password: 'password0',
-            role: 'admin'
-        };
+    it('GET /admin/users returns list of all users', () => {
         let adminToken = null;
         let expectedUsers = users.map(u => ({ name: u.name, email: u.email, role: u.role }));
 
-        return signup(admin)
+        return signin(admin)
             .then(t => adminToken = t.token)
             .then(() => request
                 .get('/admin/users')
@@ -62,18 +63,12 @@ describe.only('admin only options', () => {
             });
     });
 
-    it('/users/:email', () => {
-        let admin = {
-            name: 'admin1',
-            email: 'one@admin.com',
-            password: 'password1',
-            role: 'admin'
-        };
+    it('GET /admin/users/:email returns a user by email', () => {
         let adminToken = null;
         let myUser = users[0];
         let expectedUser = { name: myUser.name, email: myUser.email, role: myUser.role };
         
-        return signup(admin)
+        return signin(admin)
             .then(t => adminToken = t.token)
             .then(() => request
                 .get(`/admin/users/${myUser.email}`)
@@ -83,5 +78,28 @@ describe.only('admin only options', () => {
                 assert.deepEqual(res.body, expectedUser);
             });
     });
+
+    it('PATCH /admin/users/email returns updated user by email', () => {
+        let adminToken = null;
+        
+        let myUser = users[1];
+        let oldEmail = users[1].email;
+        let newEmail = 'update@user.com';
+        
+        let expectedUser = { name: myUser.name, email: newEmail, role: myUser.role };
+        
+        return signin(admin)
+            .then(t => adminToken = t.token)
+            .then(() => request
+                .patch(`/admin/users/${oldEmail}`)
+                .send({ email: newEmail })
+                .set('Authorization', adminToken)
+            )
+            .then(res => {
+                assert.deepEqual(res.body, expectedUser);
+            });
+    });
+    
+    // it('DELETE /admin/users/email', () => {})
 
 });
