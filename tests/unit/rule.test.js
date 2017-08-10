d962f378af2fc1157447e3dc61a619c7e69ae974
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-09 19:08:54 -0700

    Rules model, unit test, loader util, loading rules into db

--- tests/unit/rule.test.js
+++ tests/unit/rule.test.js
@@ -1,44 +1,44 @@
-const ruleConfig = require('../../lib/models/ruleConfig');
+const Rule = require('../../lib/models/rule');
 const assert = require('chai').assert;
 
 describe('Rule configuration unit tests', () => {
     it('validates with required fields', () => {
-        const testConfig = new ruleConfig({
+        const testRule = new Rule({
             name: 'rule1',
             status: 'on',
             description: 'This is a test rule configuration description.'
         });
-        return testConfig.validate();
+        return testRule.validate();
     });
 
     it('fails validation when required fields are missing', () => {
-        const testConfig = new ruleConfig();
+        const testRule = new Rule();
 
-        return testConfig.validate()
+        return testRule.validate()
             .then(() => { throw new Error('Expected validation error'); },
                 ({ errors }) => assert.equal(errors.name.kind && errors.status.kind && errors.description.kind, 'required'));
     });
 
     it('fails validation if not enum type', () => {
-        const testConfig = new ruleConfig({
+        const testRule = new Rule({
             name: 'something else',
             status: 'something else',
             description: 'This is a test rule configuration description.'
         });
 
-        return testConfig.validate()
+        return testRule.validate()
             .then(() => { throw new Error('Expected validation error'); },
                 ({ errors }) => assert.equal(errors.name.kind && errors.status.kind, 'enum'));
     });
 
     it('fails validation if description is over max length', () => {
-        const testConfig = new ruleConfig({
+        const testRule = new Rule({
             name: 'rule2',
             status: 'off',
             description: 'This is a really long test rule configuration description. This is a really long test rule configuration description. This is a really long test rule configuration description. This is a really long test rule configuration description. This is a really long test rule configuration description.'
         });
 
-        return testConfig.validate()
+        return testRule.validate()
             .then(() => { throw new Error('Expected validation error'); },
                 ({ errors }) => assert.equal(errors.description.kind, 'maxlength'));
     });
