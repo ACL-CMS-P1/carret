44bfb866272f6c70f35ed1cd14432f3111f7c4b5
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-10 12:43:11 -0700

    Rules tests and routes merged with existing code

---
+++ data/rules.js
@@ -0,0 +1,12 @@
+// copy and paste into robo 3t
+db.getCollection('rules').insertMany([
+    {
+        name: 'rule1',
+        active: true,
+        description: 'Requires risk score less than 80'
+    },{
+        name: 'rule2',
+        active: true,
+        description: 'Fails on three login attempts'
+    }
+]);
