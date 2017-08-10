ade6f5632e2e1691fba02ca97fc13a9089aa70f9
christylaguardia <christinelaguardia@gmail.com>
2017-08-10 09:18:49 -0700

    added status to data files

--- data/admins.js
+++ data/admins.js
@@ -4,16 +4,19 @@ db.getCollection('users').insertMany([
         name: 'Christy La Guardia',
         email: 'christy@teamcaret.com',
         password: 'chr15ty',
-        role: 'admin'
+        role: 'admin',
+        status: 'active'
     },{
         name: 'Mark Petrie',
         email: 'mark@teamcaret.com',
         password: '|v|ark!',
-        role: 'admin'
+        role: 'admin',
+        status: 'active'
     },{
         name: 'Stephanie Fitzgerald',
         email: 'stephanie@teamcaret.com',
         password: '5t3phan13',
-        role: 'admin'
+        role: 'admin',
+        status: 'active'
     }
 ]);
